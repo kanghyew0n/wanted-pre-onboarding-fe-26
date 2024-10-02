e37e9ef3ab653fd14a25a96cd33895a8b3e4d698
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-10-03 01:38:57 +0900

    ♻️ Refactor: 컴포넌트 분리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
-import { getMockData } from "./services/getMockData";
-import { MockData } from "./types/mockData";
+import { useFetchData } from "./hooks/useFetchData";
+import { useIntersectionObserver } from "./hooks/useIntersectionObserver";
 import MockItem from "./components/MockItem";
+import Header from "./components/Header";
+import SkeletonGroup from "./components/SkeletonGroup";
 
 function App() {
-  // const [page, setPage] = useState(1);
-  const [fetchedData, setFetchedData] = useState<MockData[]>([]);
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  useEffect(() => {
-    getMockData(1).then((data) => {
-      console.log(data.datas);
-      setFetchedData(data.datas);
-      setTotalPrice(data.datas.reduce((acc, cur) => acc + cur.price, 0));
-    });
-  }, []);
+  const { fetchedData, totalPrice, handlePageUpdate, isEnd, isLoading } =
+    useFetchData();
+  const { observerRef } = useIntersectionObserver(handlePageUpdate, isEnd);
 
   return (
     <main className="flex flex-col items-center justify-center pt-24">
-      <header className="w-full fixed top-0 flex justify-between items-center px-8 py-4 bg-white/50 backdrop-blur-xl">
-        <h1 className="font-bold text-2xl">challenge!</h1>
-        {totalPrice ? (
-          <h1 className="font-bold text-2xl">total price! : {totalPrice}</h1>
-        ): '스켈레톤?'}
-      </header>
+      <Header totalPrice={totalPrice} />
 
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-20">
-        {fetchedData.map((data) => (
-          <MockItem key={data.productId} data={data} />
-        ))}
-      </div>
+      {/* 초기로딩 */}
+      {isLoading && fetchedData.length === 0 ? (
+        <SkeletonGroup count={3} />
+      ) : (
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mb-8">
+          {fetchedData.map((data, idx) => (
+            <MockItem key={idx} data={data} />
+          ))}
+        </div>
+      )}
+
+      {/* 데이터가 끝나지 않았을 경우 */}
+      {!isEnd && (
+        <div ref={observerRef}>
+          <SkeletonGroup count={1} />
+        </div>
+      )}
     </main>
   );
 }
